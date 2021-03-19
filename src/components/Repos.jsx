b0db4ca5f6284525011d79739d3b79e388e1f06e
Aarni Koskela <akx@iki.fi>
2021-03-19 17:20:48 +0200

    Add statuses to projects

--- src/components/Repos.jsx
+++ src/components/Repos.jsx
@@ -1,21 +1,37 @@
 import * as React from 'react';
+import {groupBy, sortBy} from 'lodash';
 import RepoCard from './RepoCard';
 import massageReposData from '../data/reposData';
 import colors from '../data/colors.json';
 
 const data = massageReposData();
 
-function Category({ name, repos, index }) {
+const statusTiers = {
+  'Release': 1,
+  'Beta': 2,
+  'Alpha': 3,
+}
+
+function Category({name, repos, index}) {
   const color = colors[index % colors.length];
-  repos = [...repos].reverse();
+  const byStatus = groupBy([...repos].reverse(), s => s.status || 'Sketch');
+  const statusesInOrder = sortBy(Object.keys(byStatus), (s) => statusTiers[s] || 99);
+
   return (
-    <div className="category" style={{ background: color.hex }}>
-      <h2 className="category-header" style={{ color: color.hex }}>
+    <div className="category" style={{background: color.hex}}>
+      <h2 className="category-header" style={{color: color.hex}}>
         {name}
       </h2>
       <div className="category-body">
-        {repos.map((repo) => (
-          <RepoCard key={repo.name} repo={repo} />
+        {statusesInOrder.map(status => (
+          <div className="status-tier" key={status}>
+            <h3>{status}</h3>
+            <div className="status-tier-body">
+              {byStatus[status].map((repo) => (
+                <RepoCard key={repo.name} repo={repo}/>
+              ))}
+            </div>
+          </div>
         ))}
       </div>
     </div>
@@ -23,12 +39,12 @@ function Category({ name, repos, index }) {
 }
 
 export default function Repos() {
-  const { categories } = data;
+  const {categories} = data;
   const categoryList = Object.keys(categories).sort();
   return (
     <>
       {categoryList.map((category, i) => (
-        <Category name={category} repos={categories[category]} index={i} key={category} />
+        <Category name={category} repos={categories[category]} index={i} key={category}/>
       ))}
     </>
   );
